9c9d9f3b462e131b9b1c08ba95d7603aab4f295b
Jameson <jamesonexil777@gmail.com>
2024-10-02 00:19:24 -0500

    Variable and Data Types Beginning

--- Variables_Data_Types/Var_Data.py
+++ Variables_Data_Types/Var_Data.py
@@ -1,1 +1,23 @@
-#The are different types of Data Types 
+#The are different types of Data Types
+# 
+intNumber = 10
+# Variable name intNumber with the value of 10 and an int data type 
+
+floatNumber = 15.20
+# Variable name floatNumber with the value of 15.20 and a float data type
+
+myBool = True
+# Variable name myBool with a value of true and a bool data type
+
+myName = 'Jameson'
+# Variable name myName with the value of Jameson and a string data type
+
+numString = '20'
+numTypeCast = int(numString)
+# Changing the value of numString into an interger and store it a variable name numTypeCasting
+
+print(intNumber)
+print(floatNumber)
+print(myBool)
+print(myName)
+print(numTypeCast)
