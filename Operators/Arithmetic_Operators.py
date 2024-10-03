0f90ac7ff6ddcf10a98270b5ec18ffc3881c626d
Jameson <jamesonexil777@gmail.com>
2024-10-03 18:03:53 -0500

    Learning Python Continue

---
+++ Operators/Arithmetic_Operators.py
@@ -0,0 +1,68 @@
+# Arithmetic Operators = +, -, *, /, %, **, //
+
+# Use addition Operator
+carOne = 5
+# variable name carOne with a value of 5 
+carTwo = 10
+#Varialble name CarTwo with value of 10
+totalCars = carOne + carTwo
+print(totalCars)
+#Variable name totalCars with a value of carOne plus carTwo: Plus is Arithmetic Operators 
+
+
+# Use Sub Operator 
+personOne = 6
+personTwo = 10
+
+moneyMissing = personTwo - personOne
+print(moneyMissing)
+# Variable name moneyMissing with the value of personTwo minus personOne
+
+
+# Use Multi Operator
+numOne = 2
+numTwo = 2
+
+numTotal = numOne * numTwo
+print(numTotal)
+# Variable name numTotal with the value of numOne time numTwo
+
+
+# Use DIv Operator 
+divOne = 10
+divTwo = 2
+
+divTotal = divOne / divTwo
+print(divTotal)
+# Variable name divTotal with the value of divOne divided by divTwo with the decimal
+
+
+# Use Modulus Operator 
+modOne = 10
+modTwo = 4
+
+modTotal = modOne % modTwo
+print(modTotal)
+# Variable name modTotal with value of the remainder of the division of modOne and modTwo
+
+
+# Use Exponent Operator 
+expOne = 3
+expTwo = 3
+
+expTotal = expOne ** expTwo
+print(expTotal)
+# Variable name expTotal with the value of expOne to the power of ExpTwo
+
+
+# Use Floor Division Operator
+flDivOne = 13
+flDivTwo = 3
+
+flDivTotal = flDivOne // flDivTwo
+print(flDivTotal)
+# Variable name flDivTotal with the value of division without the dicimal of flDivOne and flDivTwoSS
+
+
+
+
