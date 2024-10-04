cbb8ead107d47a0ddc17f01f13bd3aba2d19672f
Jameson <jamesonexil777@gmail.com>
2024-10-03 21:34:23 -0500

    Python Learning Continue

---
+++ Operators/Assignment_Operators.py
@@ -0,0 +1,24 @@
+# Assignment Operators are use to assign value to Variables
+# Types of Assignment Operators: =, +=, -=, *=, /=, %=, **/, //=
+
+numOne = 3
+# assignning 3 to the variable numOne
+numOne += 1
+# Adding 1 to the value of the variable numOne and assignning the new value to numOne again
+print(numOne)
+
+numOne -= 2
+# Subtracting 2 to the value of the variable numOne and assignment the new value to numOne again
+print(numOne)
+
+numOne *= 5
+# Multiply 5 to the value of the variable numOne and assign the new value to numOne again
+print(numOne)
+
+numOne /= 3
+# Divide 3 to the value of the variable numOne which the result will have decimals point and asign the new value to numOne again
+print(numOne)
+
+numOne //= 1
+# Floor Divide the value of variable numOne to 1 which will not have decimals and assign the new value to numOne again 
+print(numOne)
