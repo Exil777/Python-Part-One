a13cc71000ab9915efc9d713964deaa3de054a90
Jameson <jamesonexil777@gmail.com>
2024-10-03 22:10:41 -0500

    Python Learning Continue

---
+++ Operators/Comparison_Operators.py
@@ -0,0 +1,32 @@
+# Comparison Operators compare two object. Our result is binary we either get a true or false 
+# ==, !=, >, <, >=, <=
+
+numOne = 3
+numTwo = 7
+numThree = 10
+numFour = 3
+
+isEqual = numOne == numTwo
+# Comparing numOne to see if it's equal to numTwo with the Equal Operator (==) and assign the result to isEqual variable
+print(isEqual)
+
+isNotEqual = numTwo != numThree
+# Comparing numTwo to see if it's not equal to numThree with the Not Equal Operator(!=) and assign the result to isNotEqual variable
+print(isNotEqual)
+
+isGreater = numOne > numThree
+# Comparing numOne to see if it's greater than numThree with the greater Operator (>) and assign the result to isGreater variable
+print(isGreater)
+
+isLess = numOne < numTwo
+# Comparing numOne to see if it's less than numTwo with the less than Operator (<) and assign the result to isLess variable
+print(isLess)
+
+isGreaterOrEqual = numThree >= numOne
+# Comparing numThree to see if it's greater or equal to numOne with (>=) and assign the result to isGreaterOrEqual variable
+print(isGreaterOrEqual) 
+
+isLessOrEqual = numOne <= numFour
+# Comparing numOne to see if it's less or equal to numFour with (<=) and assign the result to isLessOrEqual variable
+print(isLessOrEqual)
+
