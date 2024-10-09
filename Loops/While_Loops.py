faec72d7161a33c2442e5d2ef9c6d4d173db50c9
Jameson <jamesonexil777@gmail.com>
2024-10-08 21:54:33 -0500

    Python Learning Continue

---
+++ Loops/While_Loops.py
@@ -0,0 +1,10 @@
+# Loop is programming structure that excecutes the same code over and over again as long a certain condition is met
+
+number = 0
+
+
+while number < 10:
+    # # while keyword define the loop with a condition of the value of number is less than 10
+    number += 1
+    # if the value is less than 10: 1 is added until it reach the number 10
+    print(number)
