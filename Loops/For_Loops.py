af739f2a993b4d46a9512c2ee4d9f5c62a33d0e2
Jameson <jamesonexil777@gmail.com>
2024-10-08 22:02:40 -0500

    Python Learning Continue

---
+++ Loops/For_Loops.py
@@ -0,0 +1,7 @@
+# This loop iterate over a sequense 
+
+numbers = [10, 15, 20, 25]
+# list of numbers
+for number in numbers:
+    # the for keyword iterate over numbers and assign each value to the control variable number
+    print(number)
