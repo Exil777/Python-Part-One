1fc1419f804df181d5fb723d3144edaf058a1f04
Jameson <jamesonexil777@gmail.com>
2024-10-09 21:56:48 -0500

    Python Learning Continue

--- Loops/For_Loops.py
+++ Loops/For_Loops.py
@@ -5,3 +5,12 @@ numbers = [10, 15, 20, 25]
 for number in numbers:
     # the for keyword iterate over numbers and assign each value to the control variable number
     print(number)
+
+
+
+# Range function can create a lists that contains all numbers between two numbers
+for x in range(100):
+    print(x)
+
+for y in range(10,50):
+    print(y)
