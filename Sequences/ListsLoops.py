c5afcd9b80808f54b24e122a320ea25aa0b89b0b
Jameson <jamesonexil777@gmail.com>
2024-10-15 20:27:53 -0500

    Python Learning Continue

---
+++ Sequences/ListsLoops.py
@@ -0,0 +1,16 @@
+fruits = ["Orange", "Apple", "Banana"]
+
+# using for loop to loop through the list
+for x in fruits:
+    print(x)
+
+
+for i in range(len(fruits)):
+    print(fruits[i])
+
+
+# using while loop to loop through the list
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i = i + 1
